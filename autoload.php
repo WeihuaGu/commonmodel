633cb6278942fc6b47b64bae1e6c3d48cc06be45
weihuagu <ministarmail@msn.cn>
2016-02-24 16:19:04 +0800

    o

---
+++ autoload.php
@@ -0,0 +1,26 @@
+<?php
+//autoload.php
+
+function autoload($className)
+{
+    $className = ltrim($className, '\\');
+    $fileName  = '';
+    $namespace = '';
+    $fileclass='';
+    if ($lastNsPos = strrpos($className, '\\')) {
+        $namespace = substr($className, 0, $lastNsPos);
+        $className = substr($className, $lastNsPos + 1);
+        $fileName  = './' . str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+    $fileclass=$fileName;
+    $fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+    if (is_readable($fileName)) {
+        require $fileName;
+    }
+    $fileclass .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.class.php';
+    if (is_readable($fileclass)) {
+        require $fileclass;
+    }
+}
+
+spl_autoload_register('autoload');
