c36fd1c75b90844b30b51bf56543a3d9ff637921
weihuagu <ministarmail@msn.cn>
2016-03-01 16:59:56 +0800

    o

--- test.php
+++ test.php
@@ -1,2 +1,16 @@
 <?php 
-
+namespace Commonmodel;
+require_once 'CommonModel.php';
+$injector = new CommonModel;
+$link=[
+        'database_type' => 'mysql',
+        'database_name' => 'sns',
+        'server' => 'localhost',
+        'username' => 'root',
+        'password' => '1992',
+        'charset' => 'utf8'
+];
+$injector->share('medoo');
+$injector->define('medoo', [':options'=>$link]);
+$db = $injector->make('medoo');
+print_r( $db->info());
