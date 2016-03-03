fb1ca0208deaab12bb4a6a53fa38626453a08b76
weihuagu <ministarmail@msn.cn>
2016-03-03 16:48:30 +0800

    o

--- CommonModel.php
+++ CommonModel.php
@@ -3,8 +3,28 @@ namespace Commonmodel;
 use \Auryn\Injector;
 require_once 'vendor/autoload.php';
 class CommonModel extends  Injector {
+    var $db;
+    var $databaseconfig;
+    function __construct() {
+      parent::__construct();
+      $this->getConfig();
+    }
+    function getDatabaseConfig(){
+	return $this->databaseconfig;
+    }
+    function getConfig(){
+	$json_config = file_get_contents('config.json');
+	$config=json_decode($json_config, true);
+	$this->databaseconfig=$config['datebase'];
 
-
-
+    }
+    function  getMedoo(){
+	$this->share('medoo');
+	$this->define('medoo', [':options'=>$this->databaseconfig]);
+	$this->db = $this->make('medoo');
+	return $this->db;
+    }
+    function getPDO(){
+	return $this->db->pdo;
 
 }
--- test.php
+++ test.php
@@ -2,15 +2,5 @@
 namespace Commonmodel;
 require_once 'CommonModel.php';
 $injector = new CommonModel;
-$link=[
-        'database_type' => 'mysql',
-        'database_name' => 'sns',
-        'server' => 'localhost',
-        'username' => 'root',
-        'password' => '1992',
-        'charset' => 'utf8'
-];
-$injector->share('medoo');
-$injector->define('medoo', [':options'=>$link]);
-$db = $injector->make('medoo');
+$db = $injector->getMedoo();
 print_r( $db->info());
